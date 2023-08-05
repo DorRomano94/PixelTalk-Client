484a8ff61ad1b9ab261ef810b97db68d074743e1
Dor Romano <dorromano@outlook.com>
2023-08-05 21:16:15 +0300

    try turn server

--- src/components/VideoCall.jsx
+++ src/components/VideoCall.jsx
@@ -35,14 +35,14 @@ const VideoCall = ({ roomId }) => {
 
                 const configuration = {
                     iceServers: [
-                        { urls: 'stun:stun.l.google.com:19302' },
+                        // { urls: 'stun:stun.l.google.com:19302' },
                         {
                             url: 'turn:192.158.29.39:3478?transport=udp',
                             credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
                             username: '28224511:1379330808'
                         }
                     ]
-                };
+                }
                 const pc = new RTCPeerConnection(configuration);
 
                 stream.getTracks().forEach((track) => pc.addTrack(track, stream));
